487b2708d17cec7e225c9f337bdc01277c3ff45c
VrajSoni <vrajsoni624@gmail.com>
2026-02-24 21:48:22 +0530

    pass:JS datatype 08-paan

--- src/08-paan-shop.js
+++ src/08-paan-shop.js
@@ -46,17 +46,36 @@
  *   updatePrices({meetha:30, saada:20}, 10)              // => {meetha:40, saada:30}
  */
 export function createPaanOrder(basePaan, customizations) {
-  // Your code here
+  if(typeof basePaan !== "object" || basePaan === null || Array.isArray(basePaan)){
+    return {}
+  }
+  if(typeof customizations !== "object"|| customizations === null || Array.isArray(customizations)){
+    return {...basePaan}
+  }
+  return Object.assign({}, basePaan, customizations)
 }
 
 export function freezeMenu(menu) {
-  // Your code here
+  if(typeof menu !== "object" || menu === null || Array.isArray(menu)){
+    return {}
+  }
+  return Object.freeze(menu)
 }
 
 export function updatePrices(menu, increase) {
-  // Your code here
+  if(typeof menu !== "object" || menu === null || Array.isArray(menu) || typeof increase !== "number" || Number.isNaN(increase)){
+    return {}
+  }
+  const updatedMenu = Object.fromEntries(
+    Object.entries(menu).map(([key,value]) => {return [key, value+increase];})
+  );
+  return updatedMenu
+  
+
 }
 
 export function mergeDailySpecials(regularMenu, specialsMenu) {
-  // Your code here
+  const superMenu = (typeof regularMenu === "object" && regularMenu !== null && !Array.isArray(regularMenu)) ? regularMenu : {}
+  const superSpecialMenu = (typeof specialsMenu === "object" && specialsMenu !== null && !Array.isArray(specialsMenu)) ? specialsMenu : {}
+  return {...superMenu,...superSpecialMenu}
 }
